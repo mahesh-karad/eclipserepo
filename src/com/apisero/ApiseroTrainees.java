2c979771b3e1a09516380e349f124e39340bff07
mahesh.karad <mahesh.karad@apisero.com>
2020-11-08 23:32:50 +0530

    this is second commit

--- src/com/apisero/ApiseroTrainees.java
+++ src/com/apisero/ApiseroTrainees.java
@@ -3,7 +3,7 @@ package com.apisero;
 public class ApiseroTrainees {
 
 	public static void main(String[] args) {
-    System.out.println("hi this is mahesh karad");
+    System.out.println("hi this is mahesh karad new");
 
 	}
 
